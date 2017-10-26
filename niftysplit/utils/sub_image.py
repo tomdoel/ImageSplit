c817def5787a7929ee5f9fdc271f4914da981c38
Tom Doel <t.doel@ucl.ac.uk>
2017-10-26 19:50:02 +0100

    WIP add StoredImage class

--- niftysplit/utils/sub_image.py
+++ niftysplit/utils/sub_image.py
@@ -108,9 +108,46 @@ class SubImage(object):
 
         if self._dim_flip[abs(direction) - 1]:
             direction = - direction
-            
+
         local_coords[0] = translated[self._dim_index[0]]
         local_coords[1] = translated[self._dim_index[1]]
         local_coords[2] = translated[self._dim_index[2]]
 
         return local_coords
+
+
+class StoredImage(object):
+    """Convert image coordinates to a"""
+
+    def __init__(self, dim_order):
+
+        # Comvenience arrays for reordering dimensions
+        self._dim_index = [abs(d) - 1 for d in dim_order]
+        self._dim_flip = [1 if d < 0 else 0 for d in dim_order]
+
+
+    def write_line(self, start_coords, image_line, direction):
+        """Writes a line of image data to a binary file at the specified
+        image location """
+
+        start_coords_local, direction = self._to_file_coords(start_coords)
+        self._data_source.write_line(start_coords_local, image_line, direction)
+
+    def _to_file_coords(self, image_coords, direction):
+
+        # Flip coordinates if writing the voxels in reverse
+        flipped_coords = [x + flip*(1 + length - 2*x) for x, flip, length in
+                          zip(image_coords, self._dim_flip, self._image_size)]
+
+        # Reorder dimensions
+        file_coords = [flipped_coords[self._dim_index[x]] for x in [0, 1, 2]]
+
+        # Reorder direction
+        direction = 1 + self._dim_index.index(abs(direction) - 1)
+
+        # Flip direction
+        if self._dim_flip[abs(direction) - 1]:
+            direction = - direction
+
+
+        return file_coords, direction
