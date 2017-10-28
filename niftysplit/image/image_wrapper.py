ab17c82185188f35d12ec979ecfa22038d150e33
Tom Doel <t.doel@ucl.ac.uk>
2017-10-28 16:40:59 +0100

    Added ImageWrapper

---
+++ niftysplit/image/image_wrapper.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+import numpy as np
+
+
+class ImageWrapper(object):
+    """Multi-dimensional image array with an origin for reference to global coordinates"""
+
+    def __init__(self, origin, image_size=None, image=None):
+        self.origin = origin
+        if image is not None:
+            self.size = list(image.shape)
+        else:
+            self.size = image_size
+        self.image = image
+
+    def set_sub_image(self, sub_image):
+        """Replaces part of the image with the corresponding subimage"""
+        if self.image is None:
+            self.image = np.zeros(shape=self.size, dtype=sub_image.image.dtype)
+        start_indices = np.subtract(sub_image.origin, self.origin)
+        end_indices = np.add(start_indices, np.array(sub_image.size))
+        if np.any(np.less(start_indices, np.zeros(shape=start_indices.shape))) or \
+                np.any(np.greater(end_indices, self.size)):
+            raise ValueError("The sub image is not contained within the main image")
+        selector = tuple([slice(start, end) for start, end in zip(start_indices, end_indices)])
+        self.image[selector] = sub_image.image
---
+++ tests/test_imageWrapper.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+import numpy as np
+
+from image.image_wrapper import ImageWrapper
+import itertools
+
+
+class TestImageWrapper(TestCase):
+    def test_set_sub_image(self):
+        test_array = np.array([[1, 2, 3, 4, 5, 6], [21, 22, 23, 24, 25, 26], [31, 32, 33, 34, 35, 36], [41, 42, 43, 44, 45, 46]])
+        main_image = ImageWrapper([1, 2], image=test_array)
+        sub_image = ImageWrapper([2, 3], image=np.array([[7, 8], [9, 10]]))
+        main_image.set_sub_image(sub_image)
+        compare_array = np.array([[1, 2, 3, 4, 5, 6], [21, 7, 8, 24, 25, 26], [31, 9, 10, 34, 35, 36], [41, 42, 43, 44, 45, 46]])
+        self.assertTrue(np.array_equal(compare_array, main_image.image))
+
+        all_dim_sizes = [1, 37]
+        all_origins = [0, 17]
+        for num_dimensions in [2, 3, 4]:
+
+            multi_dimensions = [all_dim_sizes] * num_dimensions
+            multi_origins = [all_origins] * num_dimensions
+            dim_sizes_list = list(itertools.product(*multi_dimensions))
+            offsets_list = list(itertools.product(*multi_origins))
+
+            for main_dim_size in dim_sizes_list:
+                for sub_dim_size in dim_sizes_list:
+                    for main_origin in offsets_list:
+                        for sub_origin in offsets_list:
+
+                            self.test_image(num_dimensions, main_dim_size, main_origin, sub_dim_size, sub_origin)
+
+    def test_image(self, num_dimensions, main_dim_size, main_origin, sub_dim_size, sub_origin):
+        raw_array = np.reshape(np.arange(0, np.prod(main_dim_size)), main_dim_size)
+        main_image = ImageWrapper(main_origin, image=raw_array)
+        sub_raw_array = np.reshape(np.arange(1000, 1000 + np.prod(sub_dim_size)), sub_dim_size)
+        sub_image = ImageWrapper(sub_origin, image=sub_raw_array)
+        is_valid = True
+        for dim_index in range(0, num_dimensions):
+            if sub_origin[dim_index] < main_origin[dim_index] or \
+                                    sub_origin[dim_index] + sub_dim_size[dim_index] > \
+                                    main_origin[dim_index] + main_dim_size[dim_index]:
+                is_valid = False
+        if is_valid:
+            main_image.set_sub_image(sub_image)
+        else:
+            try:
+                main_image.set_sub_image(sub_image)
+                self.fail("Expeced this function call to fail")
+            except ValueError:
+                pass
+
+
