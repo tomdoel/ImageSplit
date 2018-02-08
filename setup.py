d4917f8df47421da039ef96dcb6d5ccc4dc20e60
Tom Doel <t.doel@ucl.ac.uk>
2018-02-08 13:07:57 +0000

    Updates setup files

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='ImageSplit',
     version='0.1',
-    description='Utility for splitting large image files into chunks',
-    url='https://cmiclab.cs.ucl.ac.uk/GIFT-Surg/ImageSplit',
+    description='Utility for splitting large image files into slices or chunks',
+    url='https://github.com/gift-surg/ImageSplit',
     author='Tom Doel',
     author_email='t.doel@ucl.ac.uk',
     license='BSD license',
