dd179f6beaa511d915ce51ac55efcee2efa1f1b5
Tom Doel <t.doel@ucl.ac.uk>
2018-07-06 14:25:02 +0100

    Updated linting

--- imagesplit/__main__.py
+++ imagesplit/__main__.py
@@ -13,9 +13,8 @@ if __name__ == "__main__" and not __package__:
     # To allow the package's main function to be executed without the -m switch,
     # i.e. "python packagename", we have to explicitly set the module name and
     # append the parent directory to the sys.path (see PEP 366)
-    import sys
     import os.path as path
-    __package__ = "imagesplit"
+    __package__ = "imagesplit"  # pylint: disable=redefined-builtin
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
     sys.path.append(path.dirname(path.dirname(__file__)))
 
